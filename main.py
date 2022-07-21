d2602ddc0b459b9c1280efa2a328046737aa102e
mori-dv <morimori.ph@gmail.com>
2022-07-21 15:14:49 +0430

    updating code

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 
 def binary_search(array, target):
 
+    """
+    it is good for searching in sorted array and has more efficiency than linear search
+    :param array: list of numbers
+    :param target: the number of you're following
+    :return: index of the target in array and if there isn't, return -1
+    """
+
+    array = sorted(array)
     start = 0
     end = len(array) - 1
 
@@ -14,3 +22,19 @@ def binary_search(array, target):
             start = middle + 1
 
     return -1
+
+
+def linear_search(array, target):
+
+    """
+    it is used for searching in array which is not sorted
+    :param array: list of numbers
+    :param target: the number of you're following
+    :return: index of the target in array and if there isn't, return -1
+    """
+
+    for i in range(len(array)):
+        if target == array[i]:
+            return i
+    return -1
+
